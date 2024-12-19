6532665c12dc8c420088d0a1989560d61a67b038
stevehinga <stevekamauhinga@gmail.com>
2024-12-19 17:21:17 +0300

    Modify db.php configuration

--- admin/Config/db.php
+++ admin/Config/db.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+
+$conn = mysqli_connect("localhost","root","Systemadmin@2001","isp_sys");
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+echo "Connected successfully";
+
+mysqli_close($conn);
+?>
